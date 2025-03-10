0905a0c12a79b7b9c583a569521ba501064c1bee
Ali Jan <alijandeveloper15@gmail.com>
2025-03-10 12:29:20 -0700

    element get

---
+++ script.js
@@ -0,0 +1,7 @@
+const hourHand = document.querySelector('.hour-hand');
+const minuteHand = document.querySelector('.minute-hand');
+const secondHand = document.querySelector('.second-hand');
+const digitalTime = document.querySelector('.digital-time');
+const dateDisplay = document.querySelector('.date-display');
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
